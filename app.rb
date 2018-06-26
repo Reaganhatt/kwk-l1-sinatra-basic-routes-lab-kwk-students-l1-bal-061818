7a436b77ae5c99b4893fcabb6f3ecfba77903ee6
Reaganhatt <reagan.hatt@gmail.com>
2018-06-26 14:42:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/'
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is __"
+  end
+  get '/hometown' do
+    "My hometown is"
 end
